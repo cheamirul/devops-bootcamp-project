401cae6ef83e3781ec966e9fb36719fbb2f84410
cheamirul <cheamirul0@gmail.com>
2026-01-02 10:32:52 +0800

    change key

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "devops-bootcamp-terraform-cheamirul"
-    key          = "terraform.tfstate"
+    key          = "terraform-project.tfstate"
     region       = "ap-southeast-1"
     encrypt      = true
     use_lockfile = true
